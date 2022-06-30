ef50d9fd5968b02829e706ed19f496fc809735bf
emrergin <emrergin2757@yahoo.com>
2022-06-30 18:43:17 +0300

    added lint and resolved issues came up

--- models/person.js
+++ models/person.js
@@ -2,38 +2,38 @@ const url = process.env.MONGODB_URI
 const mongoose = require('mongoose')
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: [true, 'Every person has a name']
+  name: {
+    type: String,
+    minLength: 3,
+    required: [true, 'Every person has a name']
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: [true, 'Phone number required'],
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v)
       },
-    number: { 
-        type: String,
-        minLength: 8,
-        required: [true, 'Phone number required'],
-        validate: {
-            validator: function(v) {
-              return /^\d{2,3}-\d+$/.test(v);
-            },
-            message: props => `${props.value} is not a valid phone number!`
-          },
-      },
-    })
-    
-    personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-    })
-    
+      message: props => `${props.value} is not a valid phone number!`
+    },
+  },
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
 module.exports = mongoose.model('Person', personSchema)
